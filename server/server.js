6e9ba742dd2eeaff092b65e6b53bb3d2b87ebd42
Hussam <hussam@Hussams-Mac-Pro.local>
2018-07-25 20:31:45 +0300

    emit Newmessage on CreateMessage

--- server/server.js
+++ server/server.js
@@ -13,18 +13,19 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-  
-  socket.emit('newMessage', {
-   from: 'John',
-   text: 'See you then',
-   createdAt: 123123
- });
+
+
 
  socket.on('createMessage', (message) => {
    console.log('createMessage', message);
+
+
+ socket.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
  });
-
-
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
