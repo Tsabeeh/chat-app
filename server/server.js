b12a69312aa98e79ced24358e79ef0a22403bf96
Hussam <hussam@Hussams-Mac-Pro.local>
2018-07-25 22:36:49 +0300

    create new user , and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
 
+    socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
+      createdAt: new Date().getTime()
+    });
 
  socket.on('createMessage', (message) => {
    console.log('createMessage', message);
@@ -25,6 +36,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+ // socket.broadcast.emit('newMessage', {
+ //      from: message.from,
+ //      text: message.text,
+ //      createdAt: new Date().getTime()
+ //    });
  });
   socket.on('disconnect', () => {
     console.log('User was disconnected');
