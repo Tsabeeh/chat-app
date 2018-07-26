c74a7953305dd8740d0b69dd2a1e2d8d63a86cb9
Hussam <hussam@Hussams-Mac-Pro.local>
2018-07-26 17:13:34 +0300

    add geolocaton suport from giolocation api

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
 
+const {generateMessage , generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,23 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined') );
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+  });
 
- socket.on('createMessage', (message, callback) => {
-   console.log('createMessage', message);
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
-
- io.emit('newMessage',generateMessage(message.from, message.text));
- callback('this is comming from the server');
- // socket.broadcast.emit('newMessage', {
- //      from: message.from,
- //      text: message.text,
- //      createdAt: new Date().getTime()
- //    });
- });
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
